53c14dfb1ea4dbc0946667350da1ac00bc4568f3
lachie-underhill <lachlanu@gmail.com>
2023-06-13 17:11:48 +1000

    feat: bring room title closer to center

--- src/components/RoomSection.tsx
+++ src/components/RoomSection.tsx
@@ -10,9 +10,11 @@ interface Props {
 
 export default function RoomSection(props: Props) {
 	return (
-		<div>
-			<Heading size="md">{props.room.name}</Heading>
-			<div class="flex gap-2 flex-wrap justify-evenly py-4">
+		<div class="py-8">
+			<div class="w-full max-w-6xl mx-auto">
+				<Heading size="md">{props.room.name}</Heading>
+			</div>
+			<div class="flex gap-4 gap-y-10 flex-wrap justify-evenly py-4">
 				<For each={getBundlesInRoom(props.room)}>
 					{(bundle) => <Bundle bundle={bundle} />}
 				</For>
